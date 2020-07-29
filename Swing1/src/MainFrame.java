7687e0d82eeb8af373c9e4b31a1c3eedb515327c
Justin-Chen <justin.uob@gmail.com>
2020-07-29 15:55:38 +0800

    textarea and click me

--- Swing1/src/MainFrame.java
+++ Swing1/src/MainFrame.java
@@ -1,7 +1,14 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
 
+	private JTextArea textArea;
+	private JButton btn;
+	
 	public MainFrame() {
 		// 原始
 //		JFrame frame = new JFrame("Hello Swing");
@@ -11,6 +18,15 @@ public class MainFrame extends JFrame{
 		
 		// 繼承
 		super("Hello Swing");
+		
+		setLayout(new BorderLayout());
+		
+		textArea = new JTextArea();
+		btn = new JButton("click me");
+		
+		add(textArea, BorderLayout.CENTER);
+		add(btn, BorderLayout.SOUTH);
+		
 		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
