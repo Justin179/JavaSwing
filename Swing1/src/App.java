e5b7ad28dbc3604eccec26497c87ab0773169e8e
Justin-Chen <justin.uob@gmail.com>
2020-07-29 13:35:27 +0800

    hello swing

--- Swing1/src/App.java
+++ Swing1/src/App.java
@@ -1,11 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+		// This method shouldbe used when an application thread needs to update the GUI
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+
+				JFrame frame = new JFrame("Hello Swing");
+				frame.setSize(600, 500);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				
+				
+			}
+		});
 		
-		System.out.println("git workflow done");
+		
 	}
 
 }
